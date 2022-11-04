50f7bff0f87953d1cda788d1b88f314faecdfebe
方翔鸣 <48317891+xiangming-fang@users.noreply.github.com>
2022-11-04 13:03:24 +0800

    时间截取

--- src/main/java/com/xm/jy/test/date/DateTest.java
+++ src/main/java/com/xm/jy/test/date/DateTest.java
@@ -3,6 +3,9 @@ package com.xm.jy.test.date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @ProjectName: practice
@@ -15,7 +18,7 @@ import java.time.format.DateTimeFormatter;
 public class DateTest {
 
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         LocalDate now = LocalDate.now();
         System.out.println(now.plusDays(1).equals(now.plusDays(1)));
         System.out.println(now.plusDays(1).equals(now));
@@ -24,6 +27,12 @@ public class DateTest {
         System.out.println(localDateTime.format(DateTimeFormatter.ISO_DATE_TIME));
         LocalDateTime localDateTime1 = now.atTime(23, 59, 59);
         System.out.println(localDateTime1);
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now.toLocalTime());
+        System.out.println(now.toLocalTime().truncatedTo(ChronoUnit.MINUTES));
     }
 
 }
