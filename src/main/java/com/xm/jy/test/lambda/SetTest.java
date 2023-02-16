a80d60191af042e354921d6a9941a1de195798f5
xiangming.fang <fang.albert@foxmail.com>
2023-02-16 20:22:46 +0800

    set.toArray(new String[0]) 定义一个数组类型，大小在底层会控制的

---
+++ src/main/java/com/xm/jy/test/lambda/SetTest.java
@@ -0,0 +1,24 @@
+package com.xm.jy.test.lambda;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
+/**
+ * @ProjectName: practice
+ * @Package: com.xm.jy.test.lambda
+ * @ClassName: SetTest
+ * @Author: albert.fang
+ * @Description:
+ * @Date: 2023/2/16 20:17
+ */
+public class SetTest {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("hello");
+        set.add("world");
+        String[] setArray = set.toArray(new String[0]);
+        System.out.println(setArray.length);
+        System.out.println(Arrays.stream(setArray).collect(Collectors.joining()).toString());
+    }
+}
