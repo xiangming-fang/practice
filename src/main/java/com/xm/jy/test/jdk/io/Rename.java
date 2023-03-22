65c86b4a0ac6fe4ce52d4cad6b587efef9bbc005
xiangming.fang <fang.albert@foxmail.com>
2023-03-22 22:45:01 +0800

    rename & count

--- src/main/java/com/xm/jy/test/jdk/io/Rename.java
+++ src/main/java/com/xm/jy/test/jdk/io/Rename.java
@@ -14,9 +14,37 @@ import java.util.Objects;
  * @Date: 2023/3/22 20:45
  */
 public class Rename {
-    public static void main(String[] args) throws Exception{
 
-        File file = new File("F:\\软考-系统架构设计师");
+    public static void main(String[] args) throws Exception {
+
+        String path = "F:\\软考-系统架构设计师";
+//        String path = "F:\\1--【软考】系统架构设计师-考试介绍";
+
+        System.out.printf("重命名之前有 %d 个文件\n",count(new File(path)));
+        rename(path);
+        System.out.printf("重命名之后有 %d 个文件\n",count(new File(path)));
+    }
+
+
+    public static int count(File file){
+        int res = 0;
+        if (Objects.nonNull(file.listFiles())){
+            for (File f : file.listFiles()) {
+                if (f.isDirectory()) {
+                    res += count(f);
+                }else {
+                    res ++;
+                }
+            }
+        }
+        return res;
+    }
+
+
+
+    public static void rename(String path) throws Exception{
+
+        File file = new File(path);
         if (file.isDirectory()) {
             LinkedList<File> queue = new LinkedList<>();
             queue.offer(file);
