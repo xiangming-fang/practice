e29f5b40596aea9870b40a8e7eeef4581b3c6fd7
xiangming.fang <fang.albert@foxmail.com>
2023-05-31 16:15:39 +0800

    C

---
+++ src/main/java/com/xm/jy/test/jdk/reflect/clazz/C.java
@@ -0,0 +1,17 @@
+package com.xm.jy.test.jdk.reflect.clazz;
+
+/**
+ * @Author: xiangming.fang
+ * @Date: 2023/5/22 18:41
+ */
+public class C extends B{
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    protected void getB() {
+
+    }
+}
